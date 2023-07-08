f4052041cc235dbf0ef9db70a9b4b78a6c6046bf
Rabee <ralbhaisi96@gmail.com>
2023-07-08 19:06:00 +0300

    File structure phase

---
+++ config/environment.ts
@@ -0,0 +1,31 @@
+/**
+ * This module centralize all the environment variables of the application. Thanks to this module, there MUST NOT be any
+ * `process.env` instruction in any other file or module.
+ */
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { DATABASE_URL, PORT, NODE_ENV, SECRET_KEY, ORIGIN } = process.env;
+const config = {
+  database: {
+    uri: DATABASE_URL,
+  },
+  port: PORT || 4000,
+  nodeEnv: NODE_ENV || "development",
+  jwt: {
+    secretKey: SECRET_KEY || "",
+  },
+  client: {
+    origin: ORIGIN || "http://localhost:3000",
+  },
+  ssl:
+    NODE_ENV === "production"
+      ? {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      : false,
+};
+
+export default config;
